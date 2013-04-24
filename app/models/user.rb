c0e1f26471f4447dc528582e0c6d76d19951885a
Apprentice <apprentice@devbootcampc.om>
2013-04-24 10:15:31 -0700

    added validation and pw on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_digest, :username
+  attr_accessible :email, :username, :password, :password_confirmation
+  has_secure_password
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, 
+                    format: { with: email_regex },
+                    uniqueness: true
+
+  validates :username, presence: true,
+                       uniqueness: true
+
+
+
+
+
+
 end
