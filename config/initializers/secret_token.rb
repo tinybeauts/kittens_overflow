78e8fbba885a1d95e57498b9aec43d0ed5ee31e5
Apprentice <apprentice@devbootcampc.om>
2013-04-24 09:13:09 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KittensOverflow::Application.config.secret_token = 'bf4d12c4c087a53fa59f6f4ec49802d1ef81994ee988d211ea6561dc7abeee926d2dbc5ebde969f9e70f7b167dc6bc6687d144ad1c702fd37e3202ebd4931f4d'
